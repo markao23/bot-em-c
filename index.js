1d576eb1dde8fb3203fcb8e39974c172901aa842
markao23 <markuszi3897@outlook.com>
2024-12-16 13:20:25 -0300

    tirei foi tudo

--- index.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    const links = document.getElementById('top-menu a');
-    if (links.style.display == 'flex') {
-        links.style.display = 'none'
-    } else {
-        links.style.display = 'flex'
-    }
-}
